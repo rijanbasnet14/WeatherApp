b223f7d611653321a30f40329945a2d876fcb31f
Rijan Basnet <rijanbasnet@Mac-mini.local>
2026-06-07 21:02:22 -0400

    adding weatherClient removing restTemplate

--- src/main/java/org/example/weatherapp/client/WeatherClient.java
+++ src/main/java/org/example/weatherapp/client/WeatherClient.java
@@ -2,24 +2,30 @@ package org.example.weatherapp.client;
 
 import org.example.weatherapp.dto.WeatherResponseDTO;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class WeatherClient {
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final WebClient webClient;
+
+    public WeatherClient() {
+        this.webClient = WebClient.builder()
+                .baseUrl("https://api.open-meteo.com")
+                .build();
+    }
 
     public WeatherResponseDTO getWeather(double lat, double lon) {
-
-        String url =
-                "https://api.open-meteo.com/v1/forecast"
-                        + "?latitude=" + lat
-                        + "&longitude=" + lon
-                        + "&current_weather=true";
-
-        return restTemplate.getForObject(
-                url,
-                WeatherResponseDTO.class
-        );
+        System.out.println("WeatherClient using WebClient");
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/v1/forecast")
+                        .queryParam("latitude", lat)
+                        .queryParam("longitude", lon)
+                        .queryParam("current_weather", true)
+                        .build())
+                .retrieve()
+                .bodyToMono(WeatherResponseDTO.class)
+                .block();
     }
 }
