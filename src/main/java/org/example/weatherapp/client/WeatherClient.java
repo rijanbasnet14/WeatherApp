e498ac8cd3cf66bd0d113bcd24ad4034f3c36628
rijanbasnet14 <rijandon14@gmail.com>
2026-06-09 21:20:04 -0400

    adding logger to client for debuuging

--- src/main/java/org/example/weatherapp/client/WeatherClient.java
+++ src/main/java/org/example/weatherapp/client/WeatherClient.java
@@ -4,11 +4,15 @@ import org.example.weatherapp.dto.WeatherResponseDTO;
 import org.example.weatherapp.exception.WeatherServiceException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 public class WeatherClient {
 
     private final WebClient webClient;
+    private static final Logger log =
+            LoggerFactory.getLogger(WeatherClient.class);
 
     public WeatherClient() {
         this.webClient = WebClient.builder()
@@ -18,7 +22,11 @@ public class WeatherClient {
 
     public WeatherResponseDTO getWeather(double lat, double lon) {
         try {
-            System.out.println("WeatherClient using WebClient");
+            log.info(
+                    "Fetching weather for latitude={} longitude={}",
+                    lat,
+                    lon
+            );
             return webClient.get()
                     .uri(uriBuilder -> uriBuilder
                             .path("/v1/forecast")
@@ -30,10 +38,13 @@ public class WeatherClient {
                     .bodyToMono(WeatherResponseDTO.class)
                     .block();
         } catch (Exception ex) {
+
+            log.error("Weather API call failed", ex);
+
             throw new WeatherServiceException(
                     "Failed to retrieve weather data",
-                    ex);
-
+                    ex
+            );
         }
     }
 }
